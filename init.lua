3f17ebfbacca23c68441952eaed9edb845d124b6
Daniel Bautista <dbautista9015@gmail.com>
2024-09-22 10:41:40 -0700

    Add auto-pairs, rnu, colorscheme

--- init.lua
+++ init.lua
@@ -164,7 +164,6 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 
 vim.opt.termguicolors = true
-
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
