5ea164d466ff0e61fac1adb0406a3336dcb3b010
Crypto-Spartan <29098151+Crypto-Spartan@users.noreply.github.com>
2025-03-12 17:24:44 -0400

    feat: add `vim.opt.confirm = true` (#1384)

--- init.lua
+++ init.lua
@@ -165,6 +165,11 @@ vim.opt.smartindent = true
 
 vim.opt.termguicolors = true
 
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.opt.confirm = true
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
