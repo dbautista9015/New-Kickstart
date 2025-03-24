c759222ac5d1571410512e2a5441dd96e2c942a1
Sander <info@mesander.com>
2025-03-24 20:34:57 +0100

    feat(keymap): move windows without `<C-w>` (#1368)

--- init.lua
+++ init.lua
@@ -220,6 +220,11 @@ vim.keymap.set('n', 'N', 'Nzzzv')
 
 -- Remap for when yanking a word to have that word available for paste MINE
 vim.keymap.set('x', '<leader>p', '"_dP')
+-- NOTE: Some terminals have coliding keymaps or are not able to send distinct keycodes
+-- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
+-- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
+-- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
+-- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
