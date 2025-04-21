c8e60fcf30b4f7c6c4186ecb449de5411e9bfac2
Daniel Bautista <dbautista9015@gmail.com>
2025-04-21 15:17:09 -0700

    Refactor autotag to post-depricated setup and enable autopairs

--- lua/custom/plugins/autotag.lua
+++ lua/custom/plugins/autotag.lua
@@ -1,15 +1,8 @@
 return {
   'windwp/nvim-ts-autotag',
-  event = 'BufReadPre',
-  opts = {
-    enable_close = true,
-    enable_rename = true,
-    enable_close_on_slash = false,
-  },
-
-  -- per_filetype = {
-  --   ["html"] = {
-  --     enable_close = false
-  --   }
-  -- }
+  event = 'InsertEnter',
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  config = function()
+    require('nvim-ts-autotag').setup()
+  end,
 }
